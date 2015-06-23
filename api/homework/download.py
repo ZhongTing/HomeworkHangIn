9ff187c71d80578783a2cc8623ed114f3fe33d4c
KemingChen <believe75467@gmail.com>
2015-06-23 17:28:53 +0800

    update download interface

--- api/homework/download.py
+++ api/homework/download.py
@@ -1,6 +1,6 @@
 from core.account.user_manager import user_manager
 from core.homework.homework_manager import homework_manager
-from core.utility.error_exceptions import Error, AuthorizationError
+from core.utility.error_exceptions import Error
 from core.utility.json_response import JSONResponse
 from core.utility.request_checker import RequestChecker
 from rest_framework.decorators import api_view
@@ -12,13 +12,14 @@ def download(request):
         request = RequestChecker(request)
 
         # header
-        user = user_manager.get_user_from_token(request.get_param("token"))
 
         # GET data
         data = {
+            "token": request.get_param("token"),
             "homework_id": int(request.get_param("homeworkId")),
         }
 
+        user = user_manager.get_user_from_token(data["token"])
         response = homework_manager.download_homework(data["homework_id"], user)
         return response
 
